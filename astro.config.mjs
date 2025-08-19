c0256157e12edf7ab2f7d69b053e416376a84062
vinczematyas <matyas1vincze@gmail.com>
2025-08-19 17:44:08 +0200

    font update and minor tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
@@ -7,6 +7,20 @@ export default defineConfig({
     site: 'https://vinczematyas.github.io',
     vite: {
         plugins: [tailwindcss()],
+    },
+    experimental: {
+        fonts: [
+            {
+                provider: fontProviders.google(),
+                name: "Inter",
+                cssVariable: "--font-inter"
+            },
+            {
+                provider: fontProviders.google(),
+                name: "Cascadia Mono",
+                cssVariable: "--font-cascadia-mono"
+            }
+        ]
     },
 });
 
